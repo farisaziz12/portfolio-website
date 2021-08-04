19bf1e2ff573fe1e4db487f1cd40b04a37b44554
Faris Aziz <farisaziz12@gmail.com>
2021-08-04 16:33:13 +0200

    chore(assets): add and export svg icons and profile pic

---
+++ assets/index.js
@@ -0,0 +1,55 @@
+import profilePic from "./profile-pic.png";
+import javaScriptIcon from "./icons/javascript.svg";
+import reactJsIcon from "./icons/react.svg";
+import reactRouterIcon from "./icons/react-router.svg";
+import reactiveXIcon from "./icons/reactivex.svg";
+import nextJsIcon from "./icons/nextjs.svg";
+import nodeJsIcon from "./icons/nodejs.svg";
+import html5Icon from "./icons/html-5.svg";
+import css3Icon from "./icons/css-3.svg";
+import jsonIcon from "./icons/json.svg";
+import apiIcon from "./icons/api.svg";
+import ctvIcon from "./icons/tv.svg";
+import scrumIcon from "./icons/scrum.svg";
+import rubyIcon from "./icons/ruby.svg";
+import postgreSQLIcon from "./icons/postgresql.svg";
+import relationalDBIcon from "./icons/relational.svg";
+import bashIcon from "./icons/bash.svg";
+import stripeIcon from "./icons/stripe.svg";
+import strapiIcon from "./icons/strapi.svg";
+import typescriptIcon from "./icons/typescript.svg";
+import jestIcon from "./icons/jest.svg";
+import cypressIcon from "./icons/cypress.svg";
+import gitIcon from "./icons/git.svg";
+import accessibilityIcon from "./icons/accessibility.svg";
+import visualStudioCodeIcon from "./icons/visual-studio-code.svg";
+import githubIcon from "./icons/github.svg";
+
+module.exports = {
+  profilePic,
+  javaScriptIcon,
+  reactJsIcon,
+  reactRouterIcon,
+  reactiveXIcon,
+  nextJsIcon,
+  nodeJsIcon,
+  html5Icon,
+  css3Icon,
+  jsonIcon,
+  apiIcon,
+  ctvIcon,
+  scrumIcon,
+  rubyIcon,
+  postgreSQLIcon,
+  relationalDBIcon,
+  bashIcon,
+  stripeIcon,
+  strapiIcon,
+  typescriptIcon,
+  jestIcon,
+  cypressIcon,
+  gitIcon,
+  accessibilityIcon,
+  visualStudioCodeIcon,
+  githubIcon,
+};
