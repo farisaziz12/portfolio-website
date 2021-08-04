3c15e9d7d885ab3b1725b678381d7b54d920f461
Faris Aziz <farisaziz12@gmail.com>
2021-08-04 16:35:47 +0200

    feat(intro): create intro section

---
+++ components/organisms/Intro/index.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { Link } from "react-scroll";
+import { profilePic } from "../../../assets";
+import styles from "./styles";
+
+export function Intro() {
+  useEffect(() => {
+    ScrollReveal().reveal("#intro", { delay: 100 });
+    ScrollReveal().reveal("#intro-text", { delay: 250, scale: 0.85, reset: true });
+  }, []);
+  return (
+    <div id="intro" className={styles.container}>
+      <img className={styles.img} src={profilePic.src} alt="Faris Aziz Profile Picture" />
+      <h1 id="intro-text" className={styles.title}>
+        Hi, my name is <strong>Faris!</strong>
+      </h1>
+      <h2 className={styles.subtitle}>I am a Full Stack Software Engineer</h2>
+      <Link to="learn-more" offset={-100} spy hashSpy smooth className={styles.button}>
+        Learn More!
+      </Link>
+    </div>
+  );
+}
---
+++ components/organisms/Intro/styles.js
@@ -0,0 +1,13 @@
+const container = "load-hidden grid justify-items-center";
+
+const img = "justify-self-center w-64 md:w-auto border-b-8 drop-shadow-2xl";
+
+const title = "justify-self-center text-3xl p-3 md:text-4xl lg:text-6xl";
+const subtitle = "justify-self-center text-l pb-3 md:text-xl lg:text-3xl";
+
+const button =
+  "transition ease-in mb-96 text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white mt-4 lg:mt-0 w-40 text-center cursor-pointer";
+
+const styles = { container, img, title, subtitle, button };
+
+module.exports = styles;
