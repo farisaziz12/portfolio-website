d415ad1cad933539863e35de8326400ce86357c2
Faris Aziz <farisaziz12@gmail.com>
2021-08-04 16:34:33 +0200

    style(menu): add menu styles

---
+++ components/molecules/Menu/styles.js
@@ -0,0 +1,7 @@
+const button =
+  "flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white";
+const svg = "fill-current h-6 w-6";
+
+const styles = { button, svg };
+
+module.exports = styles;
