922aa39a4f68c62f9bfb8f816e1896c28d382d74
Faris Aziz <farisaziz12@gmail.com>
2021-08-04 16:36:52 +0200

    feat(components): exports

---
+++ components/molecules/index.js
@@ -0,0 +1,4 @@
+export { GHContributionsCounter } from "./GHContributionsCounter";
+export { Menu } from "./Menu";
+export { StackIcon } from "./StackIcon";
+export { ReposList } from "./ReposList";
---
+++ components/organisms/index.js
@@ -0,0 +1,4 @@
+export { Intro } from "./Intro";
+export { LearnMore } from "./LearnMore";
+export { Navbar } from "./Navbar";
+export { Skills } from "./Skills";
