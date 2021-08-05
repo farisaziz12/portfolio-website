d8e942dcb142adac51286000154a402659b333a4
Faris Aziz <farisaziz12@gmail.com>
2021-08-05 22:42:08 +0200

    perf(stack-icon): add lazy load attribute

--- components/molecules/StackIcon/index.js
+++ components/molecules/StackIcon/index.js
@@ -4,7 +4,7 @@ import styles from "../../organisms/Skills/styles";
 export function StackIcon({ name = "", icon = { src: "" }, link = "" }) {
   return (
     <a id="skill" className={styles.skillBlock} href={link} target="_blank">
-      <img className={styles.skillIcon} src={icon.src} alt={name} />
+      <img loading="lazy" className={styles.skillIcon} src={icon.src} alt={name} />
       <h1 className={styles.skillTitle}>{name}</h1>
     </a>
   );
