87b1edb4eda38a1d98d231783c07bfd9e99488f0
Faris Aziz <farisaziz12@gmail.com>
2021-08-06 14:21:39 +0200

    perf(tv-app-demo): use next image for loading optimization

--- components/molecules/TVAppDemo/index.js
+++ components/molecules/TVAppDemo/index.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Image from "next/image";
 import { samsungTV } from "../../../assets";
 
 export function TVAppDemo() {
+  useEffect(() => {
+    ScrollReveal().reveal("#tv-demo", { delay: 250 });
+  }, []);
+
   return (
-    <div className="lg:visible invisible flex justify-center">
-      <img style={{ height: "27vw" }} className="drop-shadow-2xl" src={samsungTV.src} />
+    <div id="tv-demo" className="lg:visible invisible flex justify-center">
+      <div style={{ position: "relative", height: "27vw", width: "50vw" }}>
+        <Image
+          layout="fill"
+          objectFit="contain"
+          className="drop-shadow-2xl"
+          src={samsungTV.src}
+        />
+      </div>
       <video
         style={{
           marginTop: "2.85%",
