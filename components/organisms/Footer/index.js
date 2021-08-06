6235e7d8ce77be31eebbf7ede77eed0d21e6811b
Faris Aziz <farisaziz12@gmail.com>
2021-08-06 23:59:48 +0200

    feat(organisms): create basic footer

---
+++ components/organisms/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="w-screen border-t-4 h-24">
+      <h1 className="text-center p-2">
+        Made with<span className="ml-1 mr-2">❤️</span>by <a href={""}>Faris Aziz</a>
+      </h1>
+    </div>
+  );
+}
--- components/organisms/index.js
+++ components/organisms/index.js
@@ -3,3 +3,5 @@ export { LearnMore } from "./LearnMore";
 export { Navbar } from "./Navbar";
 export { Skills } from "./Skills";
 export { Projects } from "./Projects";
+export { Footer } from "./Footer";
+export { Services } from "./Services";
