616ccc0baf1981333136afefe4ed83ac66c437be
Faris Aziz <farisaziz12@gmail.com>
2021-08-06 14:22:23 +0200

    style(projects): add margins instead of gap

--- components/organisms/Projects/styles.js
+++ components/organisms/Projects/styles.js
@@ -2,7 +2,7 @@ const title = "place-self-start text-3xl pl-6 pb-2 md:text-4xl lg:text-6xl font-
 const subtitle = "place-self-start text-l pl-6 pb-12 md:text-xl lg:text-3xl font-light ";
 
 const button =
-  "transition ease-in inline-block text-lg px-6 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-black lg:mt-0 cursor-pointer w-56 text-center";
+  "transition ease-in inline-block text-lg px-6 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-black lg:mt-0 cursor-pointer w-56 text-center ml-2 mb-2";
 
 const privateButton =
   "inline-block text-lg px-6 py-2 leading-none border rounded text-gray-500 border-gray-500 lg:mt-0 cursor-not-allowed w-56 text-center";
