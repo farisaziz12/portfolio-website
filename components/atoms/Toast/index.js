0751a920e5afe363f00694a64996fcb17d4fbaf9
Faris Aziz <farisaziz12@gmail.com>
2021-08-07 19:45:40 +0200

    feat(toast): move class names to styles folder and add should show toast prop

--- components/atoms/Toast/index.js
+++ components/atoms/Toast/index.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
+import styles from "./styles";
 
-export function Toast({ text, embeddedFormLink, anchorTagLink, shouldUseEmbeddedForm }) {
+export function Toast({
+  text,
+  embeddedFormLink,
+  anchorTagLink,
+  shouldUseEmbeddedForm,
+  shouldShowToast,
+}) {
   const [shouldShowForm, setShouldShowForm] = useState(false);
 
   useEffect(() => {
@@ -14,12 +21,14 @@ export function Toast({ text, embeddedFormLink, anchorTagLink, shouldUseEmbedded
 
   const renderButton = () => {
     const buttonText = text || "Hire Me!";
-    const classes =
-      "load-hidden absolute z-10 text-center bg-white text-black font-bold tracking-wider text-lg px-10 py-1.5 rounded-2xl cursor-pointer mt-16";
 
     if (shouldUseEmbeddedForm) {
       return (
-        <button onClick={() => setShouldShowForm(true)} id="toast" className={classes}>
+        <button
+          onClick={() => setShouldShowForm(true)}
+          id="toast"
+          className={styles.toastButton}
+        >
           {buttonText}
         </button>
       );
@@ -30,7 +39,7 @@ export function Toast({ text, embeddedFormLink, anchorTagLink, shouldUseEmbedded
             anchorTagLink || "mailto:farisaziz12@gmail.com?subject=Faris, you're hired!"
           }
           id="toast"
-          className={classes}
+          className={styles.toastButton}
         >
           {buttonText}
         </a>
@@ -40,7 +49,7 @@ export function Toast({ text, embeddedFormLink, anchorTagLink, shouldUseEmbedded
 
   return (
     <>
-      <div className="w-full flex justify-center">{renderButton()}</div>
+      <div className={styles.toastContainer}>{shouldShowToast && renderButton()}</div>
       {shouldShowForm && (
         <Form
           closeForm={() => setShouldShowForm(false)}
@@ -53,16 +62,13 @@ export function Toast({ text, embeddedFormLink, anchorTagLink, shouldUseEmbedded
 
 function Form({ closeForm, embeddedFormLink }) {
   return (
-    <div
-      className="w-screen h-screen fixed z-20"
-      style={{ backgroundColor: "rgba(0,0,0,0.8)" }}
-    >
-      <button onClick={closeForm} className="fixed right-0 px-8 py-4 cursor-pointer z-30">
+    <div className={styles.formContainer} style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
+      <button onClick={closeForm} className={styles.formCancelButton}>
         Close
       </button>
       <iframe
         id="form"
-        className="fixed w-screen h-screen lg:p-12 pt-12 pb-2 px-2"
+        className={styles.form}
         src={embeddedFormLink || "https://form.typeform.com/to/R4OSbmBh"}
       />
     </div>
---
+++ components/atoms/Toast/styles.js
@@ -0,0 +1,14 @@
+const toastButton =
+  "load-hidden absolute z-10 text-center bg-white text-black font-bold tracking-wider text-lg px-10 py-1.5 rounded-2xl cursor-pointer mt-16";
+
+const toastContainer = "w-full flex justify-center";
+
+const formContainer = "w-screen h-screen fixed z-20";
+
+const formCancelButton = "fixed right-0 px-8 py-4 cursor-pointer z-30";
+
+const form = "fixed w-screen h-screen lg:p-12 pt-12 pb-2 px-2";
+
+const styles = { toastButton, toastContainer, formContainer, formCancelButton, form };
+
+module.exports = styles;
