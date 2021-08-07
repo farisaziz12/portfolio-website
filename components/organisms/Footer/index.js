bd96081687d75bbed2ac4ae186db8abe89e65f15
Faris Aziz <farisaziz12@gmail.com>
2021-08-07 19:44:14 +0200

    feat(footer): add links

--- components/organisms/Footer/index.js
+++ components/organisms/Footer/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { emojisplosion } from "emojisplosion";
 
-export function Footer() {
+export function Footer({ footerLinks }) {
+  const renderLinks = () => {
+    if (footerLinks[0]) {
+      return footerLinks.map(({ link, title }, index) => (
+        <a className="underline text-lg" target="_blank" href={link} key={index}>
+          {title}
+        </a>
+      ));
+    }
+  };
+
   return (
     <div className="w-screen border-t-4 h-24">
-      <h1 className="text-center p-2">
-        Made with<span className="ml-1 mr-2">❤️</span>by <a href={""}>Faris Aziz</a>
+      <h1 className="text-center p-2 cursor-pointer" onClick={emojisplosion}>
+        Made with<span className="ml-1 mr-2">❤️</span>by Faris Aziz
       </h1>
+      <div className="flex justify-evenly">{renderLinks()}</div>
     </div>
   );
 }
