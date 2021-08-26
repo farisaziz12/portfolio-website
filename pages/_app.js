0c5a0c62c3fe23af4416333f4c9f12471a16ed78
Faris Aziz <farisaziz12@gmail.com>
2021-08-26 18:56:47 +0200

    feat(app): add dark mode context provider and dev console log

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import { DarkModeProvider, DarkModeContext } from "../contexts";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    const logStyles = [
+      "color: #fff",
+      "background-color: #444",
+      "padding: 2px 4px",
+      "border-radius: 2px",
+    ].join(";");
+
+    const message =
+      "Hello Fellow Developer!\nNice to see you prying around the dev console 😉\nFeel free to give me a follow on GitHub to check out how this portfolio was built or some of the other stuff I tinker with:\nhttps://github.com/farisaziz12";
+
+    console.log(`%c${message}`, logStyles);
+  }, []);
+
+  return (
+    <DarkModeProvider>
+      <DarkModeContext.Consumer>
+        {([isDarkMode]) => <Component {...pageProps} isDarkMode={isDarkMode} />}
+      </DarkModeContext.Consumer>
+    </DarkModeProvider>
+  );
 }
 
 export default MyApp;
