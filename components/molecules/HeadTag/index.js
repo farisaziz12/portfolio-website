6c2521b6037322bbd08c403327aa961580cf5467
Faris Aziz <farisaziz12@gmail.com>
2021-08-26 21:40:31 +0200

    feat(head): remove old tracker

--- components/molecules/HeadTag/index.js
+++ components/molecules/HeadTag/index.js
@@ -2,29 +2,6 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 
 export function HeadTag() {
-  useEffect(() => {
-    // Inspectlet Asynchronous Code
-    (function () {
-      window.__insp = window.__insp || [];
-      __insp.push(["wid", 1782959115]);
-      var ldinsp = function () {
-        if (typeof window.__inspld != "undefined") return;
-        window.__inspld = 1;
-        var insp = document.createElement("script");
-        insp.type = "text/javascript";
-        insp.async = true;
-        insp.id = "inspsync";
-        insp.src =
-          ("https:" == document.location.protocol ? "https" : "http") +
-          "://cdn.inspectlet.com/inspectlet.js?wid=1782959115&r=" +
-          Math.floor(new Date().getTime() / 3600000);
-        var x = document.getElementsByTagName("script")[0];
-        x.parentNode.insertBefore(insp, x);
-      };
-      setTimeout(ldinsp, 0);
-    })();
-  }, []);
-
   return (
     <Head>
       <title>Faris Aziz - Developer Portfolio</title>
