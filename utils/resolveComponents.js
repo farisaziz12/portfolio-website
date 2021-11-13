467b67973d86db725073ca9de8192b2eb3063371
Faris Aziz <farisaziz12@gmail.com>
2021-11-13 16:39:22 +0100

    feat(resolve-components): resolve with relations

--- utils/resolveComponents.js
+++ utils/resolveComponents.js
@@ -1,30 +1,44 @@
 import { Skills, Projects, TVAppDemo, LaptopDemo, Services } from "../components";
 
 export const resolveComponents = (data, propMappings = {}) => {
-  const { component } = data;
+  const { component, id } = data;
 
   const mapProps = () => {
-    if (propMappings[component]) {
+    const componentProps = propMappings[component];
+    if (componentProps) {
+      if (Array.isArray(componentProps)) {
+        const componentPropsArr = componentProps
+          .map((prop) => {
+            if (prop?.belongsTo?.sys?.id === id) {
+              return prop;
+            }
+          })
+          .filter(Boolean);
+
+        return { [component]: componentPropsArr, ...data };
+      }
+
       return { [component]: propMappings[component], ...data };
     } else {
       return data;
     }
   };
 
+  const key = component + id;
   switch (component) {
     case "skills":
-      return <Skills {...mapProps()} key={component} />;
+      return <Skills {...mapProps()} key={key} />;
     case "projects":
-      return <Projects {...mapProps()} key={component} />;
+      return <Projects {...mapProps()} key={key} />;
     case "services":
-      return <Services {...mapProps()} key={component} />;
+      return <Services {...mapProps()} key={key} />;
     case "tv-app":
     case "tv":
-      return <TVAppDemo key={component} />;
+      return <TVAppDemo key={key} />;
     case "mac":
     case "laptop":
     case "computer":
-      return <LaptopDemo {...mapProps()} key={component} />;
+      return <LaptopDemo {...mapProps()} key={key} />;
 
     default:
       break;
