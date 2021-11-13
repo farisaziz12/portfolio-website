2a5d365a5e24d77d3bceff59f7b0f1839fba3c47
Faris Aziz <farisaziz12@gmail.com>
2021-11-13 16:38:42 +0100

    feat(resolve-icon): add icons

--- utils/resolveIcon.js
+++ utils/resolveIcon.js
@@ -38,6 +38,36 @@ export const resolveIcon = (icon) => {
       return generateIconProps(nodeJsIcon, true);
     case "rxjs":
       return generateIconProps(reactiveXIcon);
+    case "discovery-plus":
+      return {
+        src: "https://cdn.freelogovectors.net/wp-content/uploads/2020/12/discovery-plus-logo.png",
+        isDarkIcon: false,
+      };
+    case "alexa":
+      return {
+        src: "https://d3ogm7ac91k97u.cloudfront.net/en-US/alexa/branding/alexa-guidelines/brand-guidelines/the-alexa-logo.thumb.800.480.png",
+        isDarkIcon: false,
+      };
+    case "eurosport":
+      return {
+        src: "https://layout.eurosport.com/i/v8/promo_mobile/news/wen/logo.png",
+        isDarkIcon: true,
+      };
+    case "gcn":
+      return {
+        src: "https://is2-ssl.mzstatic.com/image/thumb/Purple115/v4/eb/5a/20/eb5a201a-ce5b-68b3-e9bd-7be1e6a8baa5/GCNAppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/1024x1024bb.jpg",
+        isDarkIcon: true,
+      };
+    case "raycast":
+      return {
+        src: "https://console.dev/img/interviews/raycast-petr-nikolaev-logo.png",
+        isDarkIcon: false,
+      };
+    case "skiddle":
+      return {
+        src: "https://s3-eu-west-1.amazonaws.com/skiddlecdn-general/assets/logo/png/skiddle-logo-white-stacked.png",
+        isDarkIcon: false,
+      };
     default:
       return generateIconProps(javaScriptIcon);
   }
